9588a604da60a4ff15cea82075db33921e4fb1ae
Danny Cairns <dpcairns@gmail.com>
2021-11-22 16:16:45 -0800

    Update app.js

--- app.js
+++ app.js
@@ -48,10 +48,6 @@ function setStyles() {
     boulderContainer.classList.add = 'face';
 }
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 shedButton.addEventListener('click', () => {
     const correctSpot = getRandomItem(hidingPlaces);
 
